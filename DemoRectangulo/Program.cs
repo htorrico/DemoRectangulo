06a885b854a658ee5e6940bdeb1ce97cd8d4a0c9
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2022-03-30 11:10:11 -0500

    Agregar archivos de proyecto.

---
+++ DemoRectangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRectangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese coordenadas");
+
+            Persona persona = new Persona();
+
+            persona.Nombres = "Hugo";
+            persona.Apellidos = "Torrico";
+            
+
+
+            string nombresCompletos;
+            nombresCompletos = persona.DevolverNombresCompletos();
+
+
+            Console.WriteLine(nombresCompletos);
+            
+
+            Console.Read();
+
+        }
+    }
+}
